3532ff24d6d53b28f9941c3236850688a107d15f
husskyAngel <angel.juan@correounivalle.edu.co>
2022-11-28 20:18:18 -0500

    ocho y quince

--- plot.py
+++ plot.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy as np
+import lines
 
-class Plot():
-
+class Utils():
     @staticmethod
-    def __map_points(dimx:int,dimy:int, point:tuple)->tuple:
-        px=5+ 
-        py=5+
+    def map_points(dimx:tuple,dimy:tuple, point:tuple)->tuple:
+        """
+        dimx-> tuple(maxx,minx)
+        dimy-> tuple(maxy,miny)
+        """
+        x,y=point
+        px=(x-dimx[1])  
+        py=dimy[0]-(y-dimy[1])
         return (px,py)
 
+class Plot():
 
     @staticmethod
     def drawLine(points:list):
@@ -18,10 +24,14 @@ class Plot():
         max_y,min_y=(max(points,key=lambda w: w[1])[1],min(points,key=lambda w: w[1])[1])
 
 
-        data=np.zeros((max_x-min_x+10,max_y-min_y+10))
-        data[1,1]=11
-        data[1,2]=12
-        data[0,0]=13
+        data=np.zeros((max(max_y-min_y,1),max(max_x-min_x+1,1)))
+        print(data.shape)
+        #y,x 
+        for w in points:
+            px,py=Utils.map_points((max_x,min_x),(max_y,min_y),w)
+            print("x "+str(px)+", py "+str(py))
+            data[py][px]=11
+            
 
         cmap = colors.ListedColormap(['white', 'red'])
         bounds = [1,10,20]
@@ -29,6 +39,9 @@ class Plot():
 
         fig, ax = plt.subplots()
         ax.imshow(data, cmap=cmap, norm=norm)
+        ax.set_xticks(np.arange(min_x-5, max_x+5, 1));
+        ax.set_yticks(np.arange(min_y-5,max_y+5 , 1));
+        ax.invert_yaxis()
 
         plt.show()
 
